cae000fcb07e219c21446ecf206a7e0fa0c3fb01
Jorge Alan <jorge.alann@hotmail.com>
2023-04-19 10:32:50 -0300

    Inserindo no Mongoose

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose');
+const ModelArticle = require('./ModelArticle');
 
 mongoose.connect("mongodb://localhost:27017/aprmongo");
+
+const Articles = mongoose.model("Articles", ModelArticle);
+
+const Article = new Articles({title: "Curso de NodeJs", author: "Alan", body: "Conectando DB Mongo"});
+
+Article.save();
+
+
+
+module.exports = mongoose;
